0e89b6d3ab0dd39821741b550266e118d0932598
Jeff Shen <jshen2014@hotmail.com>
2020-08-28 13:05:53 -0400

    distribution wrapper for generalization

---
+++ src/DistrWrapper.rs
@@ -0,0 +1,35 @@
+use statrs::distribution::*;
+
+#[derive(Debug, Clone, Copy)]
+pub struct DBeta;
+#[derive(Debug, Clone, Copy)]
+pub struct DNormal;
+#[derive(Debug, Clone, Copy)]
+pub struct DUniform;
+
+
+pub trait DWrapper {
+    type Distr: Continuous<f64, f64>;
+    fn new(self, param1: f64, param2: f64) -> Self::Distr;
+}
+
+impl DWrapper for DBeta {
+    type Distr = Beta;
+    fn new(self, param1: f64, param2: f64) -> Self::Distr {
+        Beta::new(param1, param2).unwrap()
+    }
+}
+
+impl DWrapper for DNormal {
+    type Distr = Normal;
+    fn new(self, param1: f64, param2: f64) -> Self::Distr {
+        Normal::new(param1, param2).unwrap()
+    }
+}
+
+impl DWrapper for DUniform {
+    type Distr = Uniform;
+    fn new(self, param1: f64, param2: f64) -> Self::Distr {
+        Uniform::new(param1, param2).unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod DistrWrapper;
 use rand::prelude::*;
 use rand_distr::Distribution;
 use statrs::distribution::{Normal, Beta, Uniform, Continuous};
@@ -8,12 +9,12 @@ use std::{
 };
 
 fn main() {
-    let n = 1000;
     let mut rng = rand::thread_rng();
-    let norm_dist_sampler = Beta::new(2., 4.).unwrap();
+    let n = 1000;
+    let norm_dist_sampler = Normal::new(2., 4.).unwrap();
     let data = norm_dist_sampler.sample_iter(&mut rng).take(n).collect::<Vec<f64>>();
 
-    // let mut dat_file = File::open("data.csv").unwrap();
+    // let dat_file = File::open("test_beta.csv").unwrap();
     // let buf = BufReader::new(dat_file);
     // let mut data = buf.lines()
     //     .map(|l| f64::from_str(&l.unwrap()).unwrap())
@@ -22,10 +23,12 @@ fn main() {
     // data = scale(data, 0., 1.);
 
     let mu_start = 1.;
-    let sigma_start = 5.;
-    let mu_prior = Normal::new(mu_start, 5.).unwrap();
-    let sigma_prior = Normal::new(sigma_start, 5.).unwrap();
-    let (mu_samples, sigma_samples) = sampler(&data, 10000, rng, mu_start, sigma_start, mu_prior, sigma_prior);
+    let sigma_start = 6.;
+    let mu_prior = Uniform::new(f64::EPSILON, 3.).unwrap();
+    let sigma_prior = Normal::new(5., 5.).unwrap();
+    let mu_distr = DistrWrapper::DNormal;
+    let sigma_distr = DistrWrapper::DNormal;
+    let (mu_samples, sigma_samples) = sampler(&data, 10000, rng, mu_distr, sigma_distr, mu_start, sigma_start, mu_prior, sigma_prior);
 
     println!("d={:?}", data);
     //data.iter().for_each(|x| println!("{}", x));
@@ -41,20 +44,38 @@ fn scale(data: Vec<f64>, low: f64, high: f64) -> Vec<f64> {
     }).collect::<Vec<_>>()
 }
 
-fn sampler<T: Continuous<f64, f64>, U: Continuous<f64, f64>>(data: &Vec<f64>, n_iter: usize, mut rng: impl Rng, mut mu_current: f64, mut sigma_current: f64, mu_prior: T, sigma_prior: U) -> (Vec<f64>, Vec<f64>) {
+fn sampler<T, U, V, W>(
+    data: &Vec<f64>, 
+    n_iter: usize, 
+    mut rng: impl Rng, 
+    distr_mu: T,
+    distr_sigma: U,
+    mut mu_current: f64, 
+    mut sigma_current: f64, 
+    mu_prior: V, 
+    sigma_prior: W) 
+-> (Vec<f64>, Vec<f64>) 
+where 
+    T: DistrWrapper::DWrapper + Copy,
+    U: DistrWrapper::DWrapper + Copy,
+    V: Continuous<f64, f64>,
+    W: Continuous<f64, f64>
+{
     let mut mu_samples: Vec<f64> = vec![0.; n_iter];
     let mut sigma_samples: Vec<f64> = vec![0.; n_iter];
     mu_samples[0] = mu_current;
     sigma_samples[0] = sigma_current;
 
-
-    let proposal_dist = Uniform::new(-0.2, 0.2).unwrap();
+    let proposal_dist = Normal::new(0.0, 0.05).unwrap();
     
     for i in 1..n_iter {
-        let mu_proposal = mu_current + proposal_dist.sample(&mut rng);
+        let mut mu_proposal = mu_current + proposal_dist.sample(&mut rng);
+        while mu_proposal <= 0. {
+            mu_proposal = mu_current + proposal_dist.sample(&mut rng);
+        }
 
-        let distr_current = Beta::new(mu_current, sigma_current).unwrap();
-        let distr_proposal = Beta::new(mu_proposal, sigma_current).unwrap();
+        let distr_current = distr_mu.new(mu_current, sigma_current);
+        let distr_proposal = distr_mu.new(mu_proposal, sigma_current);
 
         let accept = accept_reject(&distr_current, &distr_proposal, mu_current, mu_proposal, &mu_prior, data, &mut rng);
 
@@ -66,10 +87,13 @@ fn sampler<T: Continuous<f64, f64>, U: Continuous<f64, f64>>(data: &Vec<f64>, n_
 
         // 
         
-        let sigma_proposal = sigma_current + proposal_dist.sample(&mut rng);
+        let mut sigma_proposal = sigma_current + proposal_dist.sample(&mut rng);
+        while sigma_proposal <= 0. {
+            sigma_proposal = sigma_current + proposal_dist.sample(&mut rng);
+        }
         
-        let distr_current = Beta::new(mu_current, sigma_current).unwrap();
-        let distr_proposal = Beta::new(mu_current, sigma_proposal).unwrap();
+        let distr_current = distr_sigma.new(mu_current, sigma_current);
+        let distr_proposal = distr_sigma.new(mu_current, sigma_proposal);
 
         let accept = accept_reject(&distr_current, &distr_proposal, sigma_current, sigma_proposal, &sigma_prior, data, &mut rng);
 
